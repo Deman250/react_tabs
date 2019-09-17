6926c2b953ecddc4d8cdb66bf713af914b71c833
Deman250 <deman.250052@gmail.com>
2019-09-17 06:22:52 +0300

    Updates

---
+++ precache-manifest.3c80e4ded3cc6fc31fd3b8e40eef0748.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3966c469e08808b0338b4d36eec4f27b",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "77a6156557a2eca36f5f",
+    "url": "/react_tabs/static/css/main.a191e98e.chunk.css"
+  },
+  {
+    "revision": "9ec34e614d50efe45bbe",
+    "url": "/react_tabs/static/js/2.53fcb526.chunk.js"
+  },
+  {
+    "revision": "77a6156557a2eca36f5f",
+    "url": "/react_tabs/static/js/main.d0551dd5.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
